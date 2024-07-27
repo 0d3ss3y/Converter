7a0bd5ca2103334f1b671f6f10ff17f7f6d27c1f
0d3ss3y <samuelkhoali002@gmail.com>
2024-07-27 21:11:28 +0200

    Main Logic Complete

--- convertor/Currency/currency.py
+++ convertor/Currency/currency.py
@@ -1,8 +1,11 @@
 from os import system
 import sys
+import Currency.rates.exchange as exchange
+import Currency.history.history as history
+import Currency.available.available as available
 
 def options():
-    option = {1 : "Exchange Rates", 2: "Historical currency conversion", 3 : "Quit"}
+    option = {1 : "Exchange Rates", 2: "Historical currency conversion", 3:"Available Rates", 4 : "Quit"}
 
     for key,i in enumerate(option.values()):
         print(f"{key+1}) {i}")
@@ -15,7 +18,7 @@ def options():
             continue
 
         if selected_opt in option:
-            if selected_opt == 3:
+            if selected_opt == 4:
                 print("Exiting...")
                 sys.exit()
             return option[selected_opt]
@@ -30,4 +33,10 @@ def main():
 
 
 def handleProcess(opt):
-    pass
+    match opt:
+        case "Exchange Rates":
+            exchange.process()
+        case "Available Rates":
+            available.process()
+        case "Historical currency conversion":
+            history.process()
