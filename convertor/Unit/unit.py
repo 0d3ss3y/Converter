86d7c644155f505e8419f6b51e48d57480d57057
Letlotlo Khoali <154567290+0d3ss3y@users.noreply.github.com>
2024-08-02 08:42:50 +0200

    Update unit.py

--- convertor/Unit/unit.py
+++ convertor/Unit/unit.py
@@ -1,2 +1,53 @@
+import os
+import sys
+import Unit.conversion.conversion as convert
+
+def options():
+    option = {1:"Length", 
+              2:"Weight", 
+              3:"Volume", 
+              4:"Temperature",
+              5:"Area",
+              6:"Time",
+              7:"Quit"}
+    
+    codes = {
+        "Length" : "len", 
+        "Weight":"weight", 
+        "Volume":"vol", 
+        "Temperature":"temp",
+        "Area":"area",
+        "Time":"time"
+    }
+
+    for key,i in enumerate(option.values()):
+        print(f"{key+1}) {i}")
+
+    while True:
+        try:
+            selected_opt = int(input("\nEnter a number: "))
+
+            if selected_opt in option:
+                if selected_opt == 7:
+                    print("Exiting....")
+                    sys.exit()
+                opt = option[selected_opt]
+                return opt, codes[opt]
+            else:
+                print("Invalid option. Please choose a valid number")
+        except ValueError:
+            print("Please enter a valid number.")
+
+
+def handleProcess(opt,code):
+    convert.process(opt,code)
+
+
 def main():
-    pass
+    if os.name ==  "posix":
+        os.system("clear")
+    elif os.name == "nt":
+        os.system("cls")
+
+    opt,code = options()
+    handleProcess(opt.lower(),code)
