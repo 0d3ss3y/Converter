00d6f9e5bc7978e5882da5314b55d9b8eb18e301
Letlotlo Khoali <154567290+0d3ss3y@users.noreply.github.com>
2024-08-12 12:38:32 +0200

    Saver for PDF AND DOCX up

--- convertor/File/Saver/saver.py
+++ convertor/File/Saver/saver.py
@@ -1,2 +1,32 @@
-allowed_ext = {"Image" : ["JPEG","PNG", "BMP" "GIF"]}
+import os
 
+allowed_ext = {"Image" : ["JPEG","PNG", "BMP","GIF","JPG"]}
+
+def options(opt):
+    for key,values in allowed_ext.items():
+        if opt in values:
+            return display_opt(key,values,opt)
+        else:
+            print("Unknown Extension")
+    return False
+
+
+def check_opt(opt,values):
+    if opt in values:
+        return True
+    return False
+
+
+def display_opt(title,values,opt):
+        os.system("cls" if os.name == 'nt' else "clear")
+
+        print(f"{title} Options:")
+
+        for value in (values):
+            if value == opt:
+                continue
+            else:
+                print(f"- {value}")
+
+        selected_opt = input("\nSelect an conversion option: ").upper()
+        return check_opt(selected_opt,values)
