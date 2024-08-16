1e87d90d7d4914a8607538fed98878f9e37eabd6
Letlotlo Khoali <154567290+0d3ss3y@users.noreply.github.com>
2024-08-16 15:06:31 +0200

    Update saver.py

--- convertor/File/Saver/saver.py
+++ convertor/File/Saver/saver.py
@@ -7,23 +7,22 @@ allowed_ext = {"Image" :["JPEG","PNG", "BMP","GIF","JPG"],
                "VIDEO" :["MP4","AVI"]}
 
 def process(opt):
+
     for key,values in allowed_ext.items():
         if opt in values:
             return display_opt(key,values,opt)
         else:
             print("Unknown Extension")
-    return False
+    return False, None,None
 
 
 def check_opt(opt,values):
     if opt in values:
-        return True
-    return False
+        return True,opt
+    return False,None
 
 
 def display_opt(title,values,opt):
-        os.system("cls" if os.name == 'nt' else "clear")
-
         print(f"{title} Options:")
 
         for value in (values):
@@ -33,4 +32,5 @@ def display_opt(title,values,opt):
                 print(f"- {value}")
 
         selected_opt = input("\nSelect an conversion option: ").upper()
-        return check_opt(selected_opt,values)
+        checked, target = check_opt(selected_opt,values)
+        return checked,target,title
