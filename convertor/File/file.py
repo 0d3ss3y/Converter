584c9acc99a46610b2d3a424d5cfbc66f107512c
0d3ss3y <samuelkhoali002@gmail.com>
2024-08-16 21:53:04 +0200

    convertor

--- convertor/File/file.py
+++ convertor/File/file.py
@@ -7,13 +7,16 @@ def clearing():
 
 def main():
     clearing()
-    loc,name, ext = process()
+    loc,name, from_ext = process()
     clearing()
     print(f"Name : {name}")
-    print(f"Extension: {ext}\n")
+    print(f"Extension: {from_ext}\n")
 
-    check,target_ext = save.process(ext)
+    check,target_ext,category = save.process(from_ext)
     clearing()
+    # print(target_ext)
+    # print(category)
+    # print(from_ext)
         
     if check and target_ext != None:
         print(f"Converting {name} to {target_ext}")
@@ -25,17 +28,17 @@ def process():
     
     pdf_location = input("File paths: ").strip()
     
-    pdf_location = pdf_location.strip("'\"")
+    pdf_location = pdf_location.strip("'\\")
     
-    pdf_name_parts = pdf_location.split("/")
-    file_name = pdf_name_parts[-1].strip("'\"") 
+    pdf_name_parts = pdf_location.split("\\")
+    file_name = pdf_name_parts[-1].strip("'\\") 
     print(file_name)
     ext_type = file_name.split(".")
-    name = file_name.split(".")[0]
+    #name = file_name.split(".")[0]
 
     if len(ext_type) > 1:
         ext = ext_type[-1].upper().strip("'\"")  
     else:
         ext = ""
     
-    return pdf_location, name, ext
+    return pdf_location, file_name, ext
