c50e1c314fba0d3964f035a377535b433f4a3908
0d3ss3y <samuelkhoali002@gmail.com>
2024-08-18 09:51:58 +0200

    Info start

--- convertor/Device/Info/info.py
+++ convertor/Device/Info/info.py
@@ -1,2 +1,13 @@
+import psutil
+
+def cpu_count():
+    cpu_percentage = psutil.cpu_count(1,True)
+    cpu_freq = psutil.cpu_freq(True)
+    
+    print("CPU Usuage per core:")
+    for i,(percent,freq) in enumerate(zip(cpu_percentage,cpu_freq),start=1):
+        print(f"Core {i}: {percent}% \nFrequency : {freq.current} MHz")
+    
 def main():
-    pass
+    cpu_count()
+    
