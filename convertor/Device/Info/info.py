550492693dc2c0656536fcaad582bb7db2873766
0d3ss3y <samuelkhoali002@gmail.com>
2024-08-21 21:46:35 +0200

    information complete

--- convertor/Device/Info/info.py
+++ convertor/Device/Info/info.py
@@ -1,20 +1,30 @@
 import psutil
-
+import os
+import datetime
 #CPU,Network,Memory
 
-def cpu():
-    cpu_percentage = psutil.cpu_count()
-    cpu_freq = psutil.cpu_freq(True)
-    
-    print("CPU Usuage per core:")
-    print(f"Number of cores : {cpu_percentage}")
-    
-    for information in cpu_freq:
-        print(information)
-    #print(cpu_freq)
+def clearing():
+    os.system("cls" if os.name == 'nt' else "clear")
 
+def cpu():
+    #times = psutil.cpu_times(True)
+    core_count = psutil.cpu_count(logical=True)
+    usable_cores = len(psutil.Process().cpu_affinity())
+    cpu_percent = psutil.cpu_percent(interval=1, percpu=True)
+    boot_time = round(psutil.boot_time(),2)
+    translated_time = datetime.datetime.fromtimestamp(boot_time).strftime("%Y-%m-%d %H:%M:%S")
+    
+    print("CPU STATS:")
+    print(f"Number of cores: {core_count}\nUsable cores : {usable_cores}\n")
+    
+    print(f"Core Percentage Per Core:")
+    for key,percent in enumerate(cpu_percent, start=1):
+        print(f"Core {key} Percentage: {percent}")    
+    print(f"\nBoot Time: {boot_time} seconds\nLast Booting time: {translated_time}") 
+    
     
     
 def main():
+    clearing()
     cpu()
     
