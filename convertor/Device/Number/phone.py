26f1fc41a8d7c6b17dcbb47ddcb4f82b71e4d5a9
0d3ss3y <samuelkhoali002@gmail.com>
2024-08-26 22:17:11 +0200

    phone complete

--- convertor/Device/Number/phone.py
+++ convertor/Device/Number/phone.py
@@ -13,20 +13,11 @@ def clearing():
     os.system("cls" if os.name == 'nt' else "clear")
 
 def validation(number):
-    phone_number = phonenumbers.parse(number)
-    valid_number = phonenumbers.is_valid_number(phone_number)
-    valid_possible = phonenumbers.is_possible_number(phone_number)
-
-    print("Validating Number....")
-    time.sleep(3)
-    if valid_number and valid_possible:
-        print(f"\n{number} is a valid number \n{number} is a possible number")
-    elif valid_number == False and valid_possible:
-        print(f"\n{number} is not a valid number \n{number} is a possible number")
-    else:
-        print(f"\n{number} is not a valid number \n{number} is not a possible number")
-
-    return valid_number
+    try:
+        parsed_number = phonenumbers.parse(number)
+        return phonenumbers.is_valid_number(parsed_number)
+    except phonenumbers.NumberParseException:
+        return False
 
 def number_info(number):
     checking = validation(number)
@@ -37,11 +28,15 @@ def number_info(number):
         timeZone = phonenumbers.timezone.time_zones_for_number(phoneNumber)
         Carrier = phonenumbers.carrier.name_for_number(phoneNumber,"en")
         Region = phonenumbers.geocoder.description_for_number(phoneNumber, "en")
-
+        zone = ", ".join(timeZone)        
+        
         print(f"Phone Number Info\n{phoneNumber}")
         print(f"\nTimezone : {timeZone}")
         print(f"Carrier Service : {Carrier}")
         print(f"Region : {Region}")
+    else:
+        print("Invalid phone number format or number is not valid.")
+
 
 def main():
     clearing()
