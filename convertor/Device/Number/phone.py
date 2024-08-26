8f4fa451964f34a66865f4c4fe3d110075b8d14b
Letlotlo Khoali <154567290+0d3ss3y@users.noreply.github.com>
2024-08-26 13:19:50 +0200

    validation done

--- convertor/Device/Number/phone.py
+++ convertor/Device/Number/phone.py
@@ -2,11 +2,31 @@
 #2. Number Finder
 
 import os
+import phonenumbers
+import time
 
 
-def validation():
-    pass
+options = {
+    "Validate" : "Validate The Phone Number",
 
+}
+
+def clearing():
+    os.system("cls" if os.name == 'nt' else "clear")
+
+def validation(number):
+    phone_number = phonenumbers.parse(number)
+    valid_number = phonenumbers.is_valid_number(phone_number)
+    valid_possible = phonenumbers.is_possible_number(phone_number)
+
+    print("Validating Number....")
+    time.sleep(3)
+    if valid_number and valid_possible:
+        print(f"\n{number} is a valid number \n{number} is a possible number")
+    elif valid_number == False and valid_possible:
+        print(f"\n{number} is not a valid number \n{number} is a possible number")
+    else:
+        print(f"\n{number} is not a valid number \n{number} is not a possible number")
 
 def national_finder():
     pass
@@ -21,5 +41,6 @@ def number_matcher():
 
 
 def main():
-    pass
-
+    clearing()
+    number = input("> Enter a number: ")
+    validation(number)
